3669425273e5906ef08bf9419688b08b6611ad62
SirMetathyst <SirMetathyst@outlook.com>
2017-12-29 17:29:42 +0000

    Updated Docker-Extract shell script

--- Psythyst/Docker-Extract.sh
+++ Psythyst/Docker-Extract.sh
@@ -1,1 +1,5 @@
-mkdir ./Publish && docker run --rm -it psythyst/psythyst:latest cat /Psythyst/Publish/Psythyst.dll > ./Publish/Psythyst.dll
+mkdir -p ./Publish
+
+docker run --rm -it psythyst/psythyst:latest cat /Psythyst/Publish/Psythyst.dll > ./Publish/Psythyst.dll
+docker run --rm -it psythyst/psythyst:latest cat /Psythyst/Publish/Psythyst.deps.json > ./Publish/Psythyst.deps.json
+docker run --rm -it psythyst/psythyst:latest cat /Psythyst/Publish/Psythyst.pdb > ./Publish/Psythyst.pdb

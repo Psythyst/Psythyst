3ea3a3ef446a6e0798563a22d964f88cb9531ed7
SirMetathyst <SirMetathyst@outlook.com>
2017-12-29 18:25:01 +0000

    Updated Dockerfile

--- Docker-Extract.sh
+++ Docker-Extract.sh
@@ -1,5 +1,7 @@
 mkdir -p ./Publish
 
+# TODO: Replace all those docker runs... X_X
+
 docker run --rm -it psythyst/psythyst:latest cat /Psythyst/Publish/Psythyst.dll > ./Publish/Psythyst.dll
 docker run --rm -it psythyst/psythyst:latest cat /Psythyst/Publish/Psythyst.deps.json > ./Publish/Psythyst.deps.json
 docker run --rm -it psythyst/psythyst:latest cat /Psythyst/Publish/Psythyst.pdb > ./Publish/Psythyst.pdb
--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM microsoft/dotnet:sdk AS Build
-WORKDIR /Psythyst
-COPY ./Psythyst ./
+WORKDIR /
+COPY ./ ./
 RUN dotnet restore
 RUN dotnet publish -c Release -o ./Publish

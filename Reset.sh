b197014f6514863e72254ef900c618febf8cacea
SirMetathyst <SirMetathyst@outlook.com>
2018-01-18 20:59:09 +0000

    Update Build Scripts

--- Reset.sh
+++ Reset.sh
@@ -2,7 +2,6 @@
 
 TOOLS_DIR="Tools"
 BIN_DIR="Bin"
-PROTOBUILD_EXE=Protobuild.exe
 
 if [ -d $TOOLS_DIR ]; then
     rm -rf $TOOLS_DIR
@@ -10,8 +9,4 @@ fi
 
 if [ -d $BIN_DIR ]; then
     rm -rf $BIN_DIR
-fi
-
-if [ -f $PROTOBUILD_EXE ]; then
-    rm -f $PROTOBUILD_EXE
 fi
